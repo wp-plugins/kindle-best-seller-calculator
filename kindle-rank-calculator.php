494f181f0fa2c58b744da58f15a292d8f16521b2
farhanz2009 <farhanz2009@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-12 22:45:58 +0000

    go go
    git-svn-id: https://plugins.svn.wordpress.org/kindle-best-seller-calculator/tags/1.0@1179953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kindle-rank-calculator.php
+++ kindle-rank-calculator.php
@@ -23,7 +23,7 @@ function display()
 <a target="_blank" style="border-bottom: none;" href="http://kindlepreneur.com/"><?php
 echo '<img class="alignnone size-medium wp-image-7" src="' . plugins_url( '/images/plugin-single.jpg', __FILE__ ) . '" > ';
 ?></a>  
-<h4 id = "discover" style = " color: #204A83;font-family: Maven Pro; font-size: 16px; line-height: 110%; padding-top: 16px;   margin-bottom: -13px;">Discover how many books an author is selling by entering their kindle best seller rank:</h4>
+<h4 id = "discover" style = " color: #204A83;font-family: Maven Pro; font-size: 16px; line-height: 110%; padding-top: 16px; font-weight:700;  margin-bottom: -13px;">Discover how many books an author is selling by entering their kindle best seller rank:</h4>
 </div>
 <div id="change"> 
 <input style="font-size: 15px; width:100%;" id="rank-input" onkeypress='return event.charCode >= 48 && event.charCode <= 57' name="input" type="text" value="" size="30" aria-required="true" placeholder="Enter the Kindle Best Seller Rank Here" required="required">
@@ -54,7 +54,7 @@ background-color: #204a83;
 "required="required"> 
 </div>
 
-<h6 align = "center" style ="  font-size: 15px; font-family: Maven Pro;
+<h6 align = "center" style ="font-weight:700;  font-size: 15px; font-family: Maven Pro;
   padding-top: 10px;">Designed by <a href="http://kindlepreneur.com/" target="_blank" style="  text-shadow: 0px 0px #F48220;
   font-size: 15px;
   color: #F48220;">kindlepreneur.com</a>, a site devoted to teaching advanced ebook marketing techniques to authors</h6>
